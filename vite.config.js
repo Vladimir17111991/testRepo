e4beb4c75a105ac998426cce3e58901707aea145
Vladimir17111991 <vovchikzaharov1991@gmail.com>
2025-09-30 09:19:17 +0300

    commit 3

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,47 @@ import svgLoader from 'vite-svg-loader';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),
-     svgLoader()
+ base: './',
+  plugins: [
+    vue({
+      template: {
+        transformAssetUrls: {
+          includeAbsolute: false,
+        },
+      },
+    }),
+    svgLoader()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: ({name}) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')){
+            return 'assets/images/[name]-[hash][extname]';
+          }
+          
+          if (/\.css$/.test(name ?? '')){
+            return 'assets/css/[name]-[hash][extname]';
+          }
+ 
+          return 'assets/[name]-[hash][extname]';
+        },
+      }
+    }
+  },
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
+  }
 });
